2d70045b2772248e5d637d41a58a7d10143c4e91
dKurtalic <dkurtalic1@etf.unsa.ba>
2022-06-04 15:00:53 +0200

    Spirala 3, popravljen status i progres na UI

--- app/src/main/java/ba/etf/rma22/projekat/data/repositories/AnketaRepository.kt
+++ app/src/main/java/ba/etf/rma22/projekat/data/repositories/AnketaRepository.kt
@@ -2,6 +2,7 @@ package ba.etf.rma22.projekat.data.repositories
 
 import android.util.Log
 import ba.etf.rma22.projekat.data.models.Anketa
+import ba.etf.rma22.projekat.data.models.AnketaTaken
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -48,23 +49,7 @@ object AnketaRepository {
                 }
             }
             Log.v("AnketaRepo", mojeAnk.size.toString())
-            /* var listaPokusaja=ApiAdapter.retrofit.getPoceteAnkete(AccountRepository.getHash()).body()
-            var sveAnkete=getAll()
-            var mojeAnkete= mutableListOf<Anketa>()
-            if (listaPokusaja != null) {
-                for (i in listaPokusaja){
-                    if (sveAnkete != null) {
-                        for (j in sveAnkete){
-                            if (i.id==j.id){
-                                mojeAnkete.add(j)
-                            }
-                        }
 
-                    }
-                }
-            }
-
-            */
             return@withContext mojeAnk
         }
 
@@ -73,16 +58,30 @@ object AnketaRepository {
 
     suspend fun getDone(): List<Anketa>? {
         return withContext(Dispatchers.IO) {
-            var mojeAnkete = getUpisane()
-            var doneAnkete = mutableListOf<Anketa>()
-            if (mojeAnkete != null) {
-                for (i in mojeAnkete) {
-                    if (i.dajStatusAnkete() == "plava") {
-                        doneAnkete.add(i)
+
+            var pokusaji=TakeAnketaRepository.getPoceteAnkete()
+            var zavrseniPokusaji= mutableListOf<AnketaTaken>()
+            var upisaneAnkete= getUpisane()
+            var zavrseneAnkete= mutableListOf<Anketa>()
+            if (pokusaji != null) {
+                for (pokusaj in pokusaji){
+                    if (pokusaj.progres==100.0){
+                        zavrseniPokusaji.add(pokusaj)
                     }
                 }
             }
-            return@withContext doneAnkete
+
+            for (pokusaj in zavrseniPokusaji){
+                if (upisaneAnkete != null) {
+                    for (anketa in upisaneAnkete){
+                        if (anketa.id==pokusaj.anketaId){
+                            zavrseneAnkete.add(anketa)
+                        }
+                    }
+                }
+            }
+
+            return@withContext zavrseneAnkete
         }
     }
 
@@ -90,6 +89,7 @@ object AnketaRepository {
         return withContext(Dispatchers.IO) {
             var mojeAnkete = getUpisane()
             var futureAnkete = mutableListOf<Anketa>()
+
             if (mojeAnkete != null) {
                 for (i in mojeAnkete) {
                     if (i.dajStatusAnkete() == "zuta") futureAnkete.add(i)
@@ -120,25 +120,3 @@ object AnketaRepository {
         }
     }
 }
-/*
-
-   suspend fun upisiMe(istrazivanje: String, grupa: String){
-       val a = getAll().stream().filter { ank -> ank.nazivIstrazivanja == istrazivanje &&
-               ank.nazivGrupe == grupa }.toList()
-       for (ank in a){
-       if (!mojeAnkete.contains(ank))
-            mojeAnkete.add(ank)
-       }
-   }
-
-     fun getMyAnkete():List<Anketa>{
-        var konacnaLista= mutableListOf<Anketa>()
-        var sveAnkete= listOfAllSurveys
-        for (i in sveAnkete){
-            for (j in mojeAnkete){
-                if (i.naziv==j.naziv && i.nazivIstrazivanja==j.nazivIstrazivanja)
-                    konacnaLista.add(i)
-            }
-        }
-      return konacnaLista.ifEmpty { emptyList()}
-    }*/
--- app/src/main/java/ba/etf/rma22/projekat/view/AnketaAdapter.kt
+++ app/src/main/java/ba/etf/rma22/projekat/view/AnketaAdapter.kt
@@ -11,11 +11,9 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ba.etf.rma22.projekat.R
 import ba.etf.rma22.projekat.data.models.Anketa
+import ba.etf.rma22.projekat.data.models.AnketaTaken
 import ba.etf.rma22.projekat.data.repositories.TakeAnketaRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import java.time.LocalDateTime
 import java.util.*
 import kotlin.math.roundToInt
@@ -24,6 +22,8 @@ class AnketaAdapter(
     private var anketaArray:List<Anketa>,
     private val onItemClick: ((anketa:Anketa)->Unit)?):
     RecyclerView.Adapter<AnketaAdapter.AnketaViewHolder>()  {
+    var pokusaj: AnketaTaken?=null
+
 
     inner class AnketaViewHolder(pogled: View): RecyclerView.ViewHolder(pogled){
         val nazivAnkete=pogled.findViewById<TextView>(R.id.nazivAnkete)
@@ -39,25 +39,42 @@ class AnketaAdapter(
     }
 
     override fun onBindViewHolder(holder: AnketaViewHolder, position: Int) {
-            holder.nazivAnkete.text=anketaArray[position].naziv
-          if (anketaArray[position].nazivIstrazivanja==null) holder.opisTekst.text="Nepoznato istrazivanje"
-            else holder.opisTekst.text=anketaArray[position].nazivIstrazivanja
-            val statusAnkete:String= dajStatusAnkete(position)
+        holder.nazivAnkete.text = anketaArray[position].naziv
+        if (anketaArray[position].nazivIstrazivanja == null) holder.opisTekst.text =
+            "Nepoznato istrazivanje"
+        else holder.opisTekst.text = anketaArray[position].nazivIstrazivanja
 
-            val context : Context=holder.datumTekst.context
-            val id=context.resources.getIdentifier(statusAnkete,"drawable",context.packageName)
-            holder.imageView.setImageResource(id)
+        val context: Context = holder.datumTekst.context
 
-            var pom=dajProgres(anketaArray[position].progres)
-            holder.progres.progress= pom
-           postaviIzgledAnkete(statusAnkete,holder,position)
 
-        holder.itemView.setOnClickListener { onItemClick!!(anketaArray[position]) }
+        GlobalScope.launch {
+            pokusaj=null
+
+            pokusaj = async { TakeAnketaRepository.getPocetuAnketu(anketaArray[position].id) }.await()
+
+            var statusAnkete=" "
+            statusAnkete=dajStatusAnkete(position,pokusaj)
+            var pom = 0
+
+            if (pokusaj != null) {
+               pom= dajProgres(pokusaj!!.progres)
+            }
+
+           withContext(Dispatchers.Main){
+               holder.progres.progress = pom
+               postaviIzgledAnkete(statusAnkete, holder, position,pokusaj)
+               val id = context.resources.getIdentifier(statusAnkete, "drawable", context.packageName)
+               holder.imageView.setImageResource(id)
+           }
+        }
+
+            holder.itemView.setOnClickListener { onItemClick!!(anketaArray[position]) }
+
     }
-    private fun postaviIzgledAnkete(statusAnkete: String, holder: AnketaViewHolder,position:Int){
+    private fun postaviIzgledAnkete(statusAnkete: String, holder: AnketaViewHolder,position:Int,pokusaj:AnketaTaken?){
 
-        if (statusAnkete.equals("plava")) {
-            holder.datumTekst.text="Anketa urađena: "+ dajDatum(anketaArray[position].datumRada)
+        if (pokusaj!=null && statusAnkete.equals("plava")) {
+            holder.datumTekst.text="Anketa urađena: "+ dajDatum(pokusaj.datumRada)
         }
         else if (statusAnkete.equals("zuta")) {
             holder.datumTekst.text="Vrijeme aktiviranja: "+dajDatum(anketaArray[position].datumPocetak)
@@ -66,11 +83,12 @@ class AnketaAdapter(
             if (anketaArray[position].datumKraj==null) holder.datumTekst.text="Nepoznato"
             else holder.datumTekst.text="Vrijeme zatvaranja: "+dajDatum(anketaArray[position].datumKraj)
         }
-        else holder.datumTekst.text="Anketa zatvorena: "+dajDatum(anketaArray[position].datumKraj)
+        else if (statusAnkete.equals("crvena")) holder.datumTekst.text="Anketa zatvorena: "+dajDatum(anketaArray[position].datumKraj)
     }
 
-    private fun dajProgres (progres: Float):Int{
-        var p= progres * 100
+    private fun dajProgres (progres:Double):Int{
+
+        var p= progres
         if ((p%2).roundToInt()!=0) {
             p+=10
         }
@@ -94,25 +112,16 @@ class AnketaAdapter(
     }
 
 
-    private fun dajStatusAnkete(position : Int):String {
-
-               // var pokusaj = TakeAnketaRepository.getPocetuAnketu(anketaArray[position].id)
-
-                // if (pocetniStatus!="") return pocetniStatus
+    private fun dajStatusAnkete(position : Int,pokusaj:AnketaTaken?):String {
                 val kalendar: Calendar = Calendar.getInstance()
                 kalendar.set(LocalDateTime.now().year, LocalDateTime.now().monthValue, LocalDateTime.now().dayOfMonth)
                 val danasnjiDatum: Date = kalendar.time
-                if (anketaArray[position].datumRada != null) return "plava"
+
+                if (pokusaj!=null && pokusaj.datumRada!=null && pokusaj!!.progres==100.0) return "plava"
                 else if (anketaArray[position].datumPocetak!!.after(danasnjiDatum)) return "zuta"
                 else if (anketaArray[position].datumPocetak!!.before(danasnjiDatum) && anketaArray[position].datumKraj != null && anketaArray[position].datumKraj?.after(danasnjiDatum) == true) return "zelena"
                 else if (anketaArray[position].datumPocetak!!.before(danasnjiDatum) && anketaArray[position].datumKraj != null && anketaArray[position].datumKraj?.before(danasnjiDatum) == true && anketaArray[position].datumRada == null) return "crvena"
                 else return "zelena"
-
-
-        var pocetniStatus=anketaArray[position].dajStatusAnkete()
-        Log.v("AnketaAdapter","pocetniStatus: $pocetniStatus")
-
-
     }
 
     override fun getItemCount(): Int {
